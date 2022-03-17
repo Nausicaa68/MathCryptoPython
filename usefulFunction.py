ba542c10809eaa39276fb53b6ef046344bc0f5e9
Guipas68 <58084848+Guipas68@users.noreply.github.com>
2022-03-16 21:49:38 -0400

    Update usefulFunction.py

--- usefulFunction.py
+++ usefulFunction.py
@@ -21,9 +21,10 @@ def calculate_a_determinant(matrix):
 
 
 def is_invertible_in_n(matrix, n):
-    print(calculate_a_determinant(matrix), " gcd ", n, " = ",
-          math.gcd(calculate_a_determinant(matrix), n))
-    if(math.gcd(calculate_a_determinant(matrix), n) == 1):
+    deterInARing = bring_a_number_in_a_ring(calculate_a_determinant(matrix), n)
+    gcd = math.gcd(calculate_a_determinant(matrix), n)
+    print(deterInARing, " gcd ", n, " = ", gcd)
+    if(gcd == 1):
         print("la matrice est invertible")
         return True
     else:
@@ -68,104 +69,218 @@ def add_matrix_2_and_2(mat1, mat2):
 
 
 def new_A_matrix(Amat, n):
-    deter = calculate_a_determinant(Amat)
-    invertA = invert_matrix_in_a_ring(Amat, n)
+    deter = bring_a_number_in_a_ring(calculate_a_determinant(Amat), n)
+    invertA = invert_matrix_22(Amat)
+    invertAInARing = invert_matrix_in_a_ring(Amat, n)
     deterInvert = invert_a_number_in_a_ring(deter, n)
-    newMat = mult_matrix_with_a_number(invertA, deterInvert)
+
+    print("Inv de A :\n", Amat, "(-1) = ", deter, "(-1) * ", invertA,
+          " = ", deterInvert, "*", invertAInARing, " = ", end="")
+
+    newMat = mult_matrix_with_a_number(invertAInARing, deterInvert)
+
+    print(" = ", newMat)
 
     for i in range(len(newMat)):
         newMat[i] = bring_a_number_in_a_ring(newMat[i], n)
+
+    print(" = ", newMat)
 
     return newMat
 
 
 def new_B_matrix(newAmat, Bmat, n):
     newMat = mult_matrix_4_time_2(newAmat, Bmat)
+
+    print("Inv de B :\n", Bmat, "(-1) =  -1 * ",
+          newAmat, " * ", Bmat, " = -", newMat, end="")
+
     newMat = mult_matrix_with_a_number(newMat, -1)
+
+    print(" = ", newMat, end="")
 
     for i in range(len(newMat)):
         newMat[i] = bring_a_number_in_a_ring(newMat[i], n)
 
+    print(" = ", newMat)
+
     return newMat
 
 
+"""
 def decipher(text, Amatrix, Bmatrix, n):
     decipher_text = ""
     text = text.lower()
 
+    print("Deciphering : ", text, "\n")
+
     for i in range(0, len(text), 2):
         cypherMat = [ord(text[i]) - 97, ord(text[i+1]) - 97]
+
+        print(cypherMat, end="")
+
         res = mult_matrix_4_time_2(Amatrix, cypherMat)
         res = add_matrix_2_and_2(res, Bmatrix)
 
         for i in range(len(res)):
             res[i] = bring_a_number_in_a_ring(res[i], n)
             decipher_text += chr(res[i]+97)
-            print(res)
+
+        print(" -> ", res)
+
+    return decipher_text
+"""
+
+
+def transform_a_text_in_number(text):
+    textMat = []
+    text = text.lower()
+    for i in range(len(text)):
+        textMat.append((ord(text[i]) - 97))
+
+    print(text, " = ", textMat)
+
+    return textMat
+
+
+def transform_numbers_in_a_text(textMat):
+    text = ""
+
+    for i in range(len(textMat)):
+        text += ((chr(textMat[i] + 97)))
+
+    print(textMat, " = ", text)
+
+    return text
+
+
+def decipher_with_number(text, Amatrix, Bmatrix, n):
+    decipher_text = []
+
+    print("Deciphering : ", text, "\n")
+
+    for i in range(0, len(text), 2):
+        cypherMat = [text[i], text[i+1]]
+
+        print(cypherMat, end="")
+
+        res = mult_matrix_4_time_2(Amatrix, cypherMat)
+        res = add_matrix_2_and_2(res, Bmatrix)
+
+        for i in range(len(res)):
+            res[i] = bring_a_number_in_a_ring(res[i], n)
+            decipher_text.append(res[i])
+
+        print(" -> ", res)
 
     return decipher_text
 
 
-def main_deciphering_vigenere(Amat, Bmat, n):
+def main_deciphering_vigenere(Amat, Bmat, n, text):
     determinant = calculate_a_determinant(Amat)
-    print("Determinant de A : ", determinant)
-    if(is_invertible_in_n(Amat, n) == True):
-        print("Deciphering function : ")
-        invertAmat = invert_matrix_in_a_ring(Amat, n)
-        print(Amat, "-1  = ", determinant, "-1 * ", invertAmat)
-        invertDeterminant = invert_a_number_in_a_ring(determinant, n)
-        print(" = ", invertDeterminant, " * ", invertAmat)
-        newAmat = new_A_matrix(invertAmat, n)
-        newAmat = mult_matrix_with_a_number(newAmat, invertDeterminant)
-        print(" = ", newAmat)
-        
+    print("Determinant de A : ", determinant, end="")
+    determinantInN = bring_a_number_in_a_ring(determinant, n)
+    print(" =  ", determinantInN)
 
-        newBmat = mult_matrix_4_time_2(newAmat, Bmat)
-        print("Bmat = ", newBmat)
-        newBmat = mult_matrix_with_a_number(newBmat, -1)
-        print(" = ", newBmat)
+    isInvertible = is_invertible_in_n(Amat, n)
+    print(" ")
+
+    if(isInvertible):
+        newAmat = new_A_matrix(Amat, n)
+        print(" ")
+        newBmat = new_B_matrix(newAmat, Bmat, n)
+        print(" ")
+
+        decrypt = decipher_with_number(text, newAmat, newBmat, n)
+        print(decrypt)
+
+        return decrypt
 
     else:
         return 0
 
 
-print(invert_a_number_in_a_ring(63, 10))  # waiting : 7
-mat = [2, 11, 7, 8]
-print(calculate_a_determinant(mat))  # waiting : -61
-print(bring_a_number_in_a_ring(-214, 28))  # waiting : 10
-print(invert_matrix_22(mat))  # waiting : [8,-11,-7,2]
-print(invert_matrix_in_a_ring(mat, 26))  # waiting : [8,15,19,2]
+def ciphering_cesar(keyA, keyB, n, text):
+    text = text.lower()
+    encipher_text = ""
 
-Amat = [2, 7, 21, 20]
-Bmat = [12, 20]
-mat2 = [1, 2]
+    for i in range(len(text)):
+        print(text[i], " = ", (ord(text[i]) - 97), end="")
+        result = keyA * (ord(text[i]) - 97) + keyB
+        print(" = ", result, end="")
+        result = bring_a_number_in_a_ring(result, n)
+        print(" = ", result, " = ", (chr(result + 97)),  end="")
+        encipher_text += chr(result + 97)
+        print("")
 
-print(add_matrix_2_and_2(Bmat, mat2))  # waiting : [13,22]
-print(add_matrix_2_and_2(mat2, Bmat))  # waiting : [13,22]
+    print(encipher_text)
 
-print(mult_matrix_4_time_2(Amat, Bmat))  # waiting : [164,652]
-print(mult_matrix_with_a_number(Amat, 2))  # waiting : [4,14,42,40]
-
-
-N = 26
-Amat = [2, 11, 7, 8]
-Bmat = [12, 20]
-
-newAmat = new_A_matrix(Amat, N)
-newBmat = new_B_matrix(newAmat, Bmat, N)
-print(newAmat)  # waiting : [2,7,21,20]
-print(newBmat)  # waiting : [18,24]
-
-print(decipher("mivmaz", newAmat, newBmat, N))  # waiting : uuodle
+    return encipher_text
 
 
-matTest1 = [2, 13, 10, 12]
-print(is_invertible_in_n(matTest1, 15))  # waiting : true
-matTest1 = [8, 17, 14, 3]
-print(is_invertible_in_n(matTest1, 28))  # waiting : false
-matTest1 = [3, 0, 9, 0, 1, 6, 4, 3, 1]
-print(is_invertible_in_n(matTest1, 12))  # waiting : false
-matTest1 = [0, 7, 2, 3, 4, 0, 8, 0, 2]
-print(is_invertible_in_n(matTest1, 9))  # waiting : true
+def calc_deciphering_key_cesar(keyA, keyB, n):
+    decipherKeyA = invert_a_number_in_a_ring(keyA, n)
+    decipherKeyB = decipherKeyA*keyB*(-1)
+    decipherKeyB = bring_a_number_in_a_ring(decipherKeyB, n)
 
-main_deciphering_vigenere(Amat, Bmat, N)
+    return decipherKeyA, decipherKeyB
+
+
+if __name__ == "__main__":
+
+    # print(invert_a_number_in_a_ring(63, 10))  # waiting : 7
+    # mat = [2, 11, 7, 8]
+    # print(calculate_a_determinant(mat))  # waiting : -61
+    # print(bring_a_number_in_a_ring(-214, 28))  # waiting : 10
+    # print(invert_matrix_22(mat))  # waiting : [8,-11,-7,2]
+    # print(invert_matrix_in_a_ring(mat, 26))  # waiting : [8,15,19,2]
+
+    # Amat = [2, 7, 21, 20]
+    # Bmat = [12, 20]
+    # mat2 = [1, 2]
+
+    # print(add_matrix_2_and_2(Bmat, mat2))  # waiting : [13,22]
+    # print(add_matrix_2_and_2(mat2, Bmat))  # waiting : [13,22]
+
+    # print(mult_matrix_4_time_2(Amat, Bmat))  # waiting : [164,652]
+    # print(mult_matrix_with_a_number(Amat, 2))  # waiting : [4,14,42,40]
+
+    # N = 26
+    # Amat = [2, 11, 7, 8]
+    # Bmat = [12, 20]
+
+    # newAmat = new_A_matrix(Amat, N)
+    # newBmat = new_B_matrix(newAmat, Bmat, N)
+    # print(newAmat)  # waiting : [2,7,21,20]
+    # print(newBmat)  # waiting : [18,24]
+
+    # print(decipher("mivmaz", newAmat, newBmat, N))  # waiting : uuodle
+
+    # matTest1 = [2, 13, 10, 12]
+    # print(is_invertible_in_n(matTest1, 15))  # waiting : true
+    # matTest1 = [8, 17, 14, 3]
+    # print(is_invertible_in_n(matTest1, 28))  # waiting : false
+    # matTest1 = [3, 0, 9, 0, 1, 6, 4, 3, 1]
+    # print(is_invertible_in_n(matTest1, 12))  # waiting : false
+    # matTest1 = [0, 7, 2, 3, 4, 0, 8, 0, 2]
+    # print(is_invertible_in_n(matTest1, 9))  # waiting : true
+
+    N = 26
+    Amat = [2, 11, 7, 8]
+    Bmat = [12, 20]
+
+    # main_deciphering_vigenere(Amat, Bmat, N, "mivmaz")
+
+    ciphering_cesar(5, 3, 11, "decide")
+    ciphering_cesar(9, 6, 11, "bggkcdcb")
+
+    print(calc_deciphering_key_cesar(15, 20, 28))
+
+    cipher_text = transform_a_text_in_number("mivmaz")
+    print(cipher_text)
+
+    decipher_text = main_deciphering_vigenere(Amat, Bmat, N, cipher_text)
+
+    print(decipher_text)
+
+    print(transform_numbers_in_a_text(decipher_text))
