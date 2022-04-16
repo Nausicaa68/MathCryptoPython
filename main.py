c9a1817e5d2b553cb47faa772c8fd7b3a2d8299d
Guipas68 <58084848+Guipas68@users.noreply.github.com>
2022-04-16 01:09:42 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from chap2Function import *
-from chap3Function import *
+from chap2Function import main_chap2
+from chap3Function import main_chap3
 
 
 if __name__ == "__main__":
-    #main_chap2()
-    print(invert_a_number_in_a_ring(21, 125))
+    # main_chap2()
+    #print(invert_a_number_in_a_ring(21, 125))
+    main_chap3()
